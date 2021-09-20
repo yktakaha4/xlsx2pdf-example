484cf584542e6694bcdb89f19a3470ad4ff64d59
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2021-09-21 00:20:57 +0900

    書いてみる

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,94 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
+import { execSync } from "child_process";
+import {
+  mkdtemp,
+  remove,
+  ensureDir,
+  writeFile,
+  stat,
+  createReadStream,
+} from "fs-extra";
+import { tmpdir } from "os";
+import { basename, join } from "path";
+import { S3 } from "aws-sdk";
 
 interface Request {
-  name?: string;
+  inBucket?: string;
+  inKey?: string;
+  outBucket?: string;
+  outKey?: string;
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   try {
-    const { name } = JSON.parse(event.body ?? "{}") as Request;
+    const { inBucket, inKey, outBucket, outKey } = JSON.parse(
+      event.body ?? "{}"
+    ) as Request;
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: `Hello, ${(name ?? "nanashi-san").toUpperCase()} !`,
-      }),
-    };
+    if (inBucket && inKey) {
+      await main(inBucket, inKey, outBucket, outKey);
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+      };
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({}),
+      };
+    }
   } catch (e) {
     return {
       statusCode: 500,
-      body: JSON.stringify({
-        error: String(e),
-      }),
+      body: JSON.stringify({}),
     };
+  }
+};
+
+const main = async (
+  inBucket: string,
+  inKey: string,
+  outBucket?: string,
+  outKey?: string
+) => {
+  const tempDir = await mkdtemp(join(tmpdir(), "pdf-"));
+  const outDir = join(tempDir, "out");
+  await ensureDir(outDir);
+
+  try {
+    const s3 = new S3();
+    const { Body: body } = await s3
+      .getObject({
+        Bucket: inBucket,
+        Key: inKey,
+      })
+      .promise();
+
+    const inFileName = basename(inKey);
+    const inFilePath = join(tempDir, inFileName);
+    await writeFile(inFilePath, body);
+
+    execSync(`soffice --convert-to pdf --outdir "${outDir}" "${inFilePath}"`);
+
+    const outFileName = inFileName.replace(/\.\w+$/, ".pdf");
+    const outFilePath = join(tempDir, outFileName);
+    const outFileStat = await stat(outFilePath);
+    if (!outFileStat.isFile()) {
+      throw new Error(`is not file: ${outFilePath}`);
+    }
+
+    const outFileStream = createReadStream(outFilePath);
+
+    await s3
+      .putObject({
+        Bucket: outBucket ?? inBucket,
+        Key: outKey ?? `${inKey}.pdf`,
+        Body: outFileStream,
+      })
+      .promise();
+  } finally {
+    remove(tempDir);
   }
 };
